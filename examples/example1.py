4f6547ce02a66cc4c1b001a49be88be7d89b7fea
Manoah Kinnaert <manoah.k@icloud.com>
2026-08-01 16:57:25 +0200

    simplify example 1

--- examples/example1.py
+++ examples/example1.py
@@ -17,26 +17,10 @@ class MyWindow(QMainWindow):
         self.chart.add_subplot("potentiometer", "PotentiometerVoltage", "Time (ms)", "Voltage (V)", 111)
         self.chart.add_line_plot("potentiometer", 10)
        
-        self._setup_ui()
+        self.setCentralWidget(self.chart)
 
     def chart_callback(self, data):
         self.chart.update_data("potentiometer", data[0], data[1])
-
-    def _setup_ui(self):
-        mylayout = QVBoxLayout()
-
-        body_frame = QFrame()
-        body_frame.setFrameShape(QFrame.Shape.NoFrame)
-        body_frame.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.MinimumExpanding) 
-        mylayout.addWidget(body_frame)
-
-        chart_layout = QVBoxLayout()
-        chart_layout.addWidget(self.chart)
-        body_frame.setLayout(chart_layout)
-
-        central_widget = QWidget()
-        central_widget.setLayout(mylayout)
-        self.setCentralWidget(central_widget)
 
 class ArduListener(ArduinoListener):
     def __init__(self):
@@ -46,14 +30,13 @@ class ArduListener(ArduinoListener):
         self.start_time = time.time()
 
         # register callback
-        self.A0.register_callback(self.plot_data)
+        self.A0.register_callback(self.process_data)
         self.A0.enable_reporting()
 
-
-
-    def plot_data(self, value):
+    def process_data(self, value):
         time_passed = (time.time() - self.start_time) * 1000
-        self.emit_signal("voltage_data", (time_passed, value * 5)) 
+        voltage = value * 5
+        self.emit_signal("voltage_data", (time_passed, voltage)) 
 
 
 def main():
@@ -64,6 +47,7 @@ def main():
 
     ardu = ArduListener()
     ardu.add_signal("voltage_data", window.chart_callback)
+
     app.exec()
 
 
